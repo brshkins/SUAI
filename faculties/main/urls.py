4ef7ec95c7a019cb323aa5e75f02ce1da4d2155c
brshkins <wwwdigudkina@gmail.com>
2025-06-10 23:00:27 +0300

    создан "скелет" страницы с тестом

--- faculties/main/urls.py
+++ faculties/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name = 'index'),
+    path('test/', views.test_views, name='test'),
+    path('test/result/', views.test_result, name ='test_result')
 ]
--- faculties/main/views.py
+++ faculties/main/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 
+def test_views(request):
+    return render(request, 'main/test.html')
+
+def test_result(request):
+    return render(request, 'main/test_result.html')
+
